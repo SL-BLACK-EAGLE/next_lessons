6953b239526411db7e1014a7dbb988c6e3a45fdf
Chathuranga Lakmal <chathudreamshms@gmail.com>
2023-09-30 20:58:23 +0530

    Next.js 01,02,03,04 lesson

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+const About = () => {
+    return (
+        <>
+            <h1>About Page</h1>
+            <Link href="/">Go to Home Page</Link>
+        </>
+    )
+}
+export default About
